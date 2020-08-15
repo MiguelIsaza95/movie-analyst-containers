b9f7770ecb4e6a0a61122c26ad452ae444203321
MiguelIsaza95 <miguel11andres@hotmail.com>
2020-08-14 19:10:18 -0500

    Delete jenkins dns

--- Terraform/public_dns.tf
+++ Terraform/public_dns.tf
@@ -21,11 +21,3 @@ resource "aws_route53_record" "secure" {
   ttl     = 300
   records = [aws_instance.bastion.public_ip]
 }
-
-resource "aws_route53_record" "jenkins" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "ci"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.jenkins.public_ip]
-}
