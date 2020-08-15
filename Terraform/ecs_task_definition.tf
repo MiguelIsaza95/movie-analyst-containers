2363e9815d9b53620742141d4ee1da9476b8cd9a
MiguelIsaza95 <miguel11andres@hotmail.com>
2020-08-14 19:09:52 -0500

    Fix container definition

--- Terraform/ecs_task_definition.tf
+++ Terraform/ecs_task_definition.tf
@@ -1,17 +1,32 @@
 resource "aws_ecs_task_definition" "proxy" {
-  family                = "backend"
-  task_role_arn         = aws_iam_role.cluster_task_role.arn
-  container_definitions = file("${path.module}/task_definition/proxy.json")
+  family                   = "proxy"
+  container_definitions    = file("${path.module}/task_definition/proxy.json")
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  task_role_arn            = aws_iam_role.cluster_instance_role.arn
+  execution_role_arn       = aws_iam_role.cluster_task_role.arn
 }
 
 resource "aws_ecs_task_definition" "ui" {
-  family                = "frontend"
-  task_role_arn         = aws_iam_role.cluster_task_role.arn
-  container_definitions = file("${path.module}/task_definition/ui.json")
+  family                   = "frontend"
+  container_definitions    = file("${path.module}/task_definition/ui.json")
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  task_role_arn            = aws_iam_role.cluster_instance_role.arn
+  execution_role_arn       = aws_iam_role.cluster_task_role.arn
 }
 
 resource "aws_ecs_task_definition" "api" {
-  family                = "backend"
-  task_role_arn         = aws_iam_role.cluster_task_role.arn
-  container_definitions = file("${path.module}/task_definition/api.json")
+  family                   = "backend"
+  container_definitions    = file("${path.module}/task_definition/api.json")
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  task_role_arn            = aws_iam_role.cluster_instance_role.arn
+  execution_role_arn       = aws_iam_role.cluster_task_role.arn
 }
